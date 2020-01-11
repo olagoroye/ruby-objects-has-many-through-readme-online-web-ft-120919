de57df731442040d4b9c8dd92dd31c38eb9d4f87
olusola olagoroye <solaolagoroye@gmail.com>
2020-01-11 06:18:53 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -9,7 +9,9 @@ class Waiter
     @yrs_experience = yrs_experience
     @@all << self
   end
- 
+ def new_meal(customer, total, tip=0)
+    Meal.new(self, customer, total, tip)
+  end
   def self.all
     @@all
   end
